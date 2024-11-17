987205b4df8cfec1e76b697a942da7f2da606695
Rey Squillace <Reynier Squillace reynier@virginia.edu>
2024-11-17 11:48:47 -0500

    fitACF routine working; added plots. Much work to be done to improve fit residuals.

--- fitACF.py
+++ fitACF.py
@@ -12,6 +12,15 @@ def gaussian(x, sig, mu):
     curve /= max(curve)
     return curve
 
+def lorentzian(x, fwhm, mu):
+     
+    hwhm = fwhm / 2
+
+    curve = (hwhm / ((x - mu)**2 + hwhm**2 )) / np.pi
+    curve /= max(curve)
+
+    return curve
+
 def find_boundaries(A, B):
     '''
     This function takes a sorted array A and finds the two values
@@ -34,43 +43,137 @@ def find_boundaries(A, B):
     else:
         #B is between A[idx-1] and A[idx]
         return A[idx-1], A[idx]
-
-
-def get_fwhm(xs, lag_sum):
-    '''
-    This routine fits a FWHM to the central portion of a 1D ACF curve. 
     
-    '''
-    search_range = np.where(np.abs(np.arange(len(lag_sum)) / len(lag_sum) - 0.5) < 0.1)[0]
+def get_range(lag_sum, order = 10):
+     
+    search_range = np.where(np.abs(np.arange(len(lag_sum)) / \
+                                   len(lag_sum) - 0.5) < 0.1)[0]
     max_idx = np.where(lag_sum == max(lag_sum[search_range]))[0]
 
     if len(max_idx) > 1:
         max_idx = max_idx[0]
     
-    minima = sgn.argrelmin(lag_sum)
+    minima = sgn.argrelmin(lag_sum, order = order)
     low, high = find_boundaries(minima[0], max_idx)
-    lag_sum[max_idx] = (lag_sum[max_idx - 1] + lag_sum[max_idx + 1]) / 2
-    lag_sum /= lag_sum[max_idx]
-    sig, mu = opt.curve_fit(gaussian, xs[low:high + 1], lag_sum[low:high+1], 
-                        [0.05*xs[-1], 0.5*xs[-1]])[0]
-    
-    fwhm_conv = 2*np.sqrt(2 * np.log(2))
+    #make the upper limit inclusive
+    high += 1
 
-    return fwhm_conv * sig, mu
+    return low, high, max_idx
+
+def get_stamp(acf, time_lag, freq_lag, plot = True):
+    ''' 
+    This function finds the "stamp" of the ACF and recalculates each 
+
+    '''
+
+    time_acf = np.sum(acf, axis = 0)
+    freq_acf = np.sum(acf, axis = 1)
+
+    tlow, thigh, tmax = get_range(time_acf)
+    flow, fhigh, fmax = get_range(freq_acf)
+
+    time_acf = np.sum(acf[flow:fhigh], axis = 0)
+    freq_acf = np.sum(acf[:, tlow:thigh], axis = 1)
+    time_lags = np.linspace(-time_lag, time_lag, len(time_acf))
+    freq_lags = np.linspace(-freq_lag, freq_lag, len(freq_acf))
+
+    if plot:
+
+        plt.imshow(acf[flow:fhigh, tlow:thigh],
+                    aspect = 0.05,
+                    cmap = 'magma',
+                    extent = [time_lags[tlow], time_lags[thigh],
+                              freq_lags[flow], freq_lags[fhigh]],
+         )
+        plt.title('ACF Stamp')
+        plt.xlabel('Time Lag (hr)')
+        plt.ylabel('Frequency Lag (MHz)')
+
+    return time_acf, time_lags, freq_acf, freq_lags
+
+def get_fwhm(xs, lag_sum, plot = True, lagtype = None):
+    '''
+    This routine fits a FWHM to the central portion of a 1D ACF curve. 
+
+    '''
+
+    if len(xs) != len(lag_sum):
+        raise ValueError(f"Arrays must have the same length. \n\
+            Got lengths {len(xs)} and {len(lag_sum)}.")
+        return
+    
+    low, high, max_idx = get_range(lag_sum)
+    #lag_sum[max_idx] = (lag_sum[max_idx - 1] + lag_sum[max_idx + 1]) / 2
+    lag_sum /= lag_sum[max_idx]
+
+    par_gauss = opt.curve_fit(gaussian, xs[low:high], lag_sum[low:high], 
+                        p0 = [0.02*xs[high], 0])
+    sig, mu_gauss = par_gauss[0][0], par_gauss[0][1]
+    cov_gauss = par_gauss[1]
+    fwhm_conv = 2*np.sqrt(2 * np.log(2))
+    fwhm_gauss = fwhm_conv * sig
+    #there is no way this is legal:
+    perr_gauss = np.sqrt(np.sum(np.diag(cov_gauss)**2))
+
+    par_lorentz = opt.curve_fit(lorentzian, xs[low:high], lag_sum[low:high], 
+                                                p0 = [0.02*xs[high], 0])
+    fwhm_lorentz, mu_lorentz = par_lorentz[0][0], par_lorentz[0][1]
+    cov_lorentz = par_lorentz[1]
+    #what the fuck am i doing here
+    perr_lorentz = np.sqrt(np.sum(np.diag(cov_lorentz))**2)
+
+    if plot:
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (12, 4))
+        ax1.set_title('Full ACF')
+        ax1.plot(xs, lag_sum, 'k')
+        ax1.plot(xs[low:high], lag_sum[low:high], 'r', label = 'Fit Range')
+        ax1.plot(xs[low:high], gaussian(xs[low:high], sig, mu_gauss), 'b--', 
+                 label = 'Gaussian Fit')
+        ax1.plot(xs[low:high], lorentzian(xs[low:high], fwhm_lorentz, 
+                 mu_lorentz), 'g--', label = 'Lorentzian Fit')
+        
+        ax2.set_title('Central Region')
+        ax2.plot(xs[low:high], lag_sum[low:high], 'r', label = 'Fit Range')
+        ax2.plot(xs[low:high], gaussian(xs[low:high], sig, mu_gauss), 'b--', 
+                 label = 'Gaussian Fit')
+        ax2.plot(xs[low:high], lorentzian(xs[low:high], fwhm_lorentz, 
+                 mu_lorentz), 'g--', label = 'Lorentzian Fit')
+
+        if lagtype in ('time', 'Time', 't'):
+                fig.suptitle('1D Time ACF')
+                ax1.set_xlabel('Time Lag (hrs)')
+                ax2.set_xlabel('Time Lag (hrs)')
+
+        elif lagtype in ('Frequency', 'freq', 'frequency', 'f'):
+                fig.suptitle('1D Frequency ACF')
+                ax1.set_xlabel('Frequency Lag (MHz)')
+                ax2.set_xlabel('Frequency Lag (MHz)')
+
+        ax1.set_ylabel('Intensity')
+        plt.legend()
+
+    if perr_gauss < perr_lorentz:   
+        print(f'Best fit for {lagtype} was a Gaussian.')
+        return fwhm_gauss, mu_gauss
+    else:
+        print(f'Best fit for {lagtype} was a Lorentzian.')
+        return fwhm_lorentz, mu_lorentz
 
 
 def fit1D(ds, hrs, freqs):
     
     acf = ds.getACF()
-    hrs = hrs[-1] - hrs[0]
-    freqs = freqs[-1] - freqs[0]
+    time_lag = hrs[-1] - hrs[0]
+    freq_lag = freqs[-1] - freqs[0]
+    time_acf, time_lags, freq_acf, freq_lags = get_stamp(acf, time_lag, freq_lag)
 
-    #sum over full acf for now
-    time_lag = np.sum(acf, axis = 0)
-    freq_lag = np.sum(acf, axis = 1)
+    dt, t0 = get_fwhm(time_lags, time_acf, lagtype = 'time')
+    dnu, nu0 = get_fwhm(freq_lags, freq_acf, lagtype = 'frequency')
 
-    dt, t0 = get_fwhm(hrs, time_lag)
-    dnu, nu0 = get_fwhm(freqs, freq_lag)
+    print(f'dt = {dt} hrs, dnu = {dnu} MHz')
+
+    return dt, t0, dnu, nu0
 
 
 
